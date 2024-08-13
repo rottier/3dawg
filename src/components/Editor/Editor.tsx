2eb6f466dd4b229c3741b965acf4d6efb88404ee
Marino Rottier <marino@rottier.nl>
2024-08-14 00:23:39 +0200

    chore: create working synth

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,58 +1,74 @@
-import { useCallback } from 'react';
+import { useCallback } from "react";
 import {
   ReactFlow,
   Controls,
   useNodesState,
   useEdgesState,
   addEdge,
-} from '@xyflow/react';
- 
-import '@xyflow/react/dist/style.css';
-import './Editor.css'
-import { AudioGraph, AudioGraphNodes } from '../../core/AudioGraph';
+} from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+import "./Editor.css";
+import { AudioGraph, AudioGraphNodes } from "../../core/AudioGraph";
 
 const audioGraph = new AudioGraph(new AudioContext());
-const nodeId1  = audioGraph.addAudioNode(AudioGraphNodes.Oscillator);
+const nodeId1 = audioGraph.addAudioNode(AudioGraphNodes.Oscillator);
 const nodeId2 = audioGraph.addAudioNode(AudioGraphNodes.Gain);
 const nodeId3 = audioGraph.addAudioNode(AudioGraphNodes.Output);
 
+audioGraph.linkNodes(nodeId1, nodeId2);
+audioGraph.linkNodes(nodeId2, nodeId3);
+
 const initialNodes = [
-  { id: nodeId1, position: { x: 0, y: 0 }, data: { label: 'Oscillator', audioNode: audioGraph.nodes[0]} },
-  { id: nodeId2, position: { x: 0, y: 100 }, data: { label: 'Gain', audioNode: audioGraph.nodes[1] } },
-  { id: nodeId3, position: { x: 0, y: 200 }, data: { label: 'Output', audioNode: audioGraph.nodes[2] } },
+  {
+    id: nodeId1,
+    position: { x: 0, y: 0 },
+    data: { label: "Oscillator", audioNode: audioGraph.nodes[0] },
+  },
+  {
+    id: nodeId2,
+    position: { x: 0, y: 100 },
+    data: { label: "Gain", audioNode: audioGraph.nodes[1] },
+  },
+  {
+    id: nodeId3,
+    position: { x: 0, y: 200 },
+    data: { label: "Output", audioNode: audioGraph.nodes[2] },
+  },
 ];
 
-audioGraph.linkNodes(nodeId1, nodeId2)
-
-const initialEdges = [{ id: 'e1-2', source: nodeId1, target: nodeId2 }, { id: 'e2-3', source: nodeId2, target: nodeId3 }];
+const initialEdges = [
+  { id: "e1-2", source: nodeId1, target: nodeId2 },
+  { id: "e2-3", source: nodeId2, target: nodeId3 },
+];
 const proOptions = { hideAttribution: true };
- 
+
 export default function Editor() {
   const [nodes, _, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
+
   const onConnect = useCallback(
     (params: any) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
+    [setEdges]
   );
- 
+
   return (
     <>
-    <button onClick={() => audioGraph.play()}>Start</button>
-    <div className={`w-full h-full bg-black bg-opacity-30`}>
-      <ReactFlow
-        className='text-blue'
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        proOptions={proOptions}
-        colorMode='light'
-      >
-        <Controls />
-      </ReactFlow>
-    </div>
+      <button onClick={() => {audioGraph.play()}}>Start</button>
+      <div className={`w-full h-full bg-black bg-opacity-30`}>
+        <ReactFlow
+          className="text-blue"
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          proOptions={proOptions}
+          colorMode="light"
+        >
+          <Controls />
+        </ReactFlow>
+      </div>
     </>
   );
 }
--- src/core/AudioGraph/AudioGraph.ts
+++ src/core/AudioGraph/AudioGraph.ts
@@ -1,5 +1,5 @@
 import { AudioGraphLink, AudioGraphNodes } from ".";
-import { uniqueId } from 'lodash';
+import { uniqueId } from "lodash";
 
 export type AddAudioNode = (type: AudioGraphNodes) => string;
 export type RemoveAudioNode = (id: string) => boolean;
@@ -7,161 +7,217 @@ export type LinkNodes = (fromId: string, toId: string) => boolean;
 export type UnlinkNodes = (fromId: string, toId: string) => boolean;
 export type FindLink = (fromId: string, toId: string) => number;
 
-export class AudioGraphNode<Node = AudioNode, Parameters = Record<string, any>> {
-    protected readonly context: AudioContext;
-    public readonly id: string;
-    public node: Node | undefined;
-    public readonly linkedFrom: () => AudioGraphNode[] = () => this.graph.links.filter((link) => link.to.id === this.id).map((link) => link.to);
-    public readonly linkedTo: () => AudioGraphNode[] = () => this.graph.links.filter((link) => link.from.id === this.id).map((link) => link.from);
-    public parameters: Partial<Parameters>;
-    protected playing: boolean;
-    public get isPlaying() { return this.playing }
-    private graph: AudioGraph;
+export class AudioGraphNode<
+  Node = AudioNode,
+  Parameters = Record<string, any>
+> {
+  protected readonly context: AudioContext;
+  public readonly id: string;
+  public node: Node | undefined;
+  public readonly linkedFrom: () => AudioGraphNode[] = () =>
+    this.graph.links
+      .filter((link) => link.to.id === this.id)
+      .map((link) => link.from);
+  public readonly linkedTo: () => AudioGraphNode[] = () =>
+    this.graph.links
+      .filter((link) => link.from.id === this.id)
+      .map((link) => link.to);
+  public parameters: Partial<Parameters>;
+  protected playing: boolean;
+  public get isPlaying() {
+    return this.playing;
+  }
+  private graph: AudioGraph;
 
-    start = () => {
-        this.onStart(); this.linkedTo().forEach((node) => {
-            node.node && (this.node as AudioNode)?.connect(node.node);
-        });
-        this.playing = true;
-    };
-    stop = () => { this.onStop(); this.playing = false; };
-    onStart = () => { };
-    onStop = () => { };
+  start() {
+    if (this.playing) this.stop();
 
-    constructor(context: AudioContext, graph: AudioGraph) {
-        this.graph = graph;
-        this.context = context;
-        this.id = uniqueId();
-        this.playing = false;
-        this.parameters = {};
+    this.beforeStart();
+
+    const linkedTo = this.linkedTo();
+
+    for (let i = 0; i < linkedTo.length; i++) {
+      const element = linkedTo[i];
+
+      if (
+        this.node instanceof AudioNode &&
+        element.node instanceof AudioNode &&
+        "connect" in this.node
+      ) {
+        this.node.connect(element.node!);
+      }
     }
+
+    this.playing = true;
+
+    this.onStart();
+  }
+
+  stop = () => {
+    if (!this.playing) return;
+
+    this.beforeStop();
+    this.playing = false;
+    this.onStop();
+  };
+  beforeStart = () => {};
+  onStart = () => {};
+  beforeStop = () => {};
+  onStop = () => {};
+
+  constructor(context: AudioContext, graph: AudioGraph) {
+    this.graph = graph;
+    this.context = context;
+    this.id = uniqueId();
+    this.playing = false;
+    this.parameters = {};
+  }
 }
 
-export class AudioGraphNodeOscillator extends AudioGraphNode<OscillatorNode, OscillatorOptions> {
-    onStart = () => this.node = new OscillatorNode(this.context, this.parameters);
-    onStop = () => this.node?.stop();
+export class AudioGraphNodeOscillator extends AudioGraphNode<
+  OscillatorNode,
+  OscillatorOptions
+> {
+  beforeStart = () =>
+    (this.node = new OscillatorNode(this.context, this.parameters));
+  onStart = () => this.node?.start();
+  onStop = () => this.node?.stop();
 
-    constructor(context: AudioContext, graph: AudioGraph) {
-        super(context, graph);
-        this.parameters = {
-            detune: 0,
-            frequency: 3000,
-            periodicWave: undefined,
-            type: 'sawtooth'
-        };
-    }
+  constructor(context: AudioContext, graph: AudioGraph) {
+    super(context, graph);
+    this.parameters = {
+      detune: 0,
+      frequency: 440,
+      periodicWave: undefined,
+      type: "sawtooth",
+    };
+    this.node = new OscillatorNode(context, this.parameters);
+  }
 }
 
 export class AudioGraphNodeGain extends AudioGraphNode<GainNode, GainOptions> {
-    onStart = () => this.node = new GainNode(this.context, this.parameters);
+  beforeStart = () => (this.node = new GainNode(this.context, this.parameters));
 
-    constructor(context: AudioContext, graph: AudioGraph) {
-        super(context, graph);
-        this.parameters = {
-            gain: 200,
-        }
-    }
+  constructor(context: AudioContext, graph: AudioGraph) {
+    super(context, graph);
+    this.parameters = {
+      gain: 1,
+    };
+    this.node = new GainNode(context, this.parameters);
+  }
 }
 
-export class AudioGraphNodeOutput extends AudioGraphNode<AudioContext, GainOptions> {
-    constructor(context: AudioContext, graph: AudioGraph) {
-        super(context, graph);
-    }
-}
+export class AudioGraphNodeOutput extends AudioGraphNode<
+  AudioDestinationNode,
+  GainOptions
+> {
+  beforeStart = () => (this.node = this.context.destination);
 
+  constructor(context: AudioContext, graph: AudioGraph) {
+    super(context, graph);
+    this.node = this.context.destination;
+  }
+}
 
 export class AudioGraph {
-    play = () => {
-        this.audioContext.resume();
-        this.nodes.forEach((node) => node.start());
-
-            console.log("Should be playing...")
+  play() {
+    console.log(this.audioContext.suspend())
+    if (this.audioContext.state === "suspended") {
+      this.audioContext.resume();
     }
-    private audioContext: AudioContext;
-    public readonly nodes: AudioGraphNode[];
-    public readonly links: AudioGraphLink[];
-    addAudioNode: AddAudioNode = (type) => {
-        let newNode: AudioGraphNode;
+    this.nodes.forEach((node) => node.start());
+  }
+  private audioContext: AudioContext;
+  public readonly nodes: AudioGraphNode[];
+  public readonly links: AudioGraphLink[];
+  addAudioNode: AddAudioNode = (type) => {
+    let newNode: AudioGraphNode;
 
-        switch (type) {
-            case AudioGraphNodes.Oscillator:
-                newNode = new AudioGraphNodeOscillator(this.audioContext, this)
-                break
-            case AudioGraphNodes.Gain:
-                newNode = new AudioGraphNodeGain(this.audioContext, this)
-                break
-            case AudioGraphNodes.Output:
-                newNode = new AudioGraphNodeOutput(this.audioContext, this) as any;
-                break
-            default:
-                throw (`Could not add audio graph node, type unknown: ${type}`)
+    switch (type) {
+      case AudioGraphNodes.Oscillator:
+        newNode = new AudioGraphNodeOscillator(this.audioContext, this);
+        break;
+      case AudioGraphNodes.Gain:
+        newNode = new AudioGraphNodeGain(this.audioContext, this);
+        break;
+      case AudioGraphNodes.Output:
+        newNode = new AudioGraphNodeOutput(this.audioContext, this) as any;
+        break;
+      default:
+        throw `Could not add audio graph node, type unknown: ${type}`;
+    }
+
+    this.nodes.push(newNode);
+    return newNode.id;
+  };
+
+  removeAudioNode: RemoveAudioNode = (id) => {
+    const i = this.nodes.findIndex((node) => node.id === id);
+    if (i !== -1) {
+      this.nodes.splice(i, 1);
+
+      // Also remove links when removing a node
+      for (let j = this.links.length - 1; j > -1; j--) {
+        const link = this.links[j];
+        if (id === link.from.id || id === link.to.id) {
+          this.links.splice(j, 1);
         }
+      }
 
-        this.nodes.push(newNode);
-
-        return newNode.id;
-    };
-
-    removeAudioNode: RemoveAudioNode = (id) => {
-        const i = this.nodes.findIndex(node => node.id === id);
-        if (i !== -1) {
-            this.nodes.splice(i, 1);
-
-            // Also remove links when removing a node
-            for (let j = this.links.length - 1; j > -1; j--) {
-                const link = this.links[j];
-                if (id === link.from.id || id === link.to.id) {
-                    this.links.splice(j, 1)
-                }
-            }
-
-            return true;
-        }
-
-        return false;
+      return true;
     }
 
-    linkNodes: LinkNodes = (fromId, toId) => {
-        let fromNode = null;
-        let toNode = null;
+    return false;
+  };
 
-        if (this.findLinkIndex(fromId, toId) > -1)
-            return true;
+  linkNodes: LinkNodes = (fromId, toId) => {
+    let fromNode = null;
+    let toNode = null;
 
-        for (const node of this.nodes) {
-            if (node.id === fromId) {
-                fromNode = node;
-            }
-            if (node.id === toId) {
-                toNode = node;
-            }
-            if (fromNode && toNode) {
-                this.links.push({ id: uniqueId(), from: fromNode, to: toNode, muted: false });
-                return true;
-            }
-        }
+    if (this.findLinkIndex(fromId, toId) > -1) return true;
 
-        return false;
+    for (const node of this.nodes) {
+      if (node.id === fromId) {
+        fromNode = node;
+      }
+      if (node.id === toId) {
+        toNode = node;
+      }
+      if (fromNode && toNode) {
+        this.links.push({
+          id: uniqueId(),
+          from: fromNode,
+          to: toNode,
+          muted: false,
+        });
+        return true;
+      }
     }
 
-    findLinkIndex: FindLink = (fromId, toId) => this.links.findIndex((link) => link.from.id === fromId && link.to.id === toId);
+    return false;
+  };
 
-    unlinkNodes: UnlinkNodes = (fromId, toId) => {
-        let linkIndex = this.findLinkIndex(fromId, toId);
-        let success = false;
+  findLinkIndex: FindLink = (fromId, toId) =>
+    this.links.findIndex(
+      (link) => link.from.id === fromId && link.to.id === toId
+    );
 
-        while (linkIndex > -1) {
-            this.links.splice(linkIndex, 1);
-            success = true;
-            linkIndex = this.findLinkIndex(fromId, toId);
-        }
+  unlinkNodes: UnlinkNodes = (fromId, toId) => {
+    let linkIndex = this.findLinkIndex(fromId, toId);
+    let success = false;
 
-        return success;
+    while (linkIndex > -1) {
+      this.links.splice(linkIndex, 1);
+      success = true;
+      linkIndex = this.findLinkIndex(fromId, toId);
     }
 
-    constructor(context: AudioContext) {
-        this.nodes = [];
-        this.links = [];
-        this.audioContext = context;
-    }
+    return success;
+  };
+
+  constructor(context: AudioContext) {
+    this.nodes = [];
+    this.links = [];
+    this.audioContext = context;
+  }
 }
