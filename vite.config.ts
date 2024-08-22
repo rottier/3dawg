7384fc862e18440f530934ac8fb3b96fed81156d
Marino Rottier <marino@rottier.nl>
2024-08-22 17:04:15 +0200

    chore: add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm()],
+  base: "./",
   resolve: {
     alias: {
       "@wasm": path.resolve(__dirname, "./dist/wasm"),
